73711d32c6386f112196ee9c08ffbc54680659b4
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-09 13:49:12 +0700

    style: set file to default

--- generator/solution.cpp
+++ generator/solution.cpp
@@ -5,18 +5,5 @@ int main()
 {
     ifstream cin("input.txt");
     ofstream cout("output.txt");
-    int n;
-    vector<int> a;
-    cin >> n;
-    a.resize(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum += a[i];
-    }
-    cout << sum << endl;
+    
 }
